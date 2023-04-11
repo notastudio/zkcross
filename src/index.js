1f4a6cfe9f663ef2eebc7d9c4cf3eba11b7aa36f
notastudio <18502810+notastudio@users.noreply.github.com>
2023-04-12 00:17:30 +0800

    add tests for different bit lengthes; add timer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const snarkjs = require('snarkjs');
 const RsaAcc = require('./RsaAcc');
 const { mkMimc, numberToBase, numberToArray, arrayPad } = require('./helper.js');
@@ -6,17 +7,52 @@ const { mod, isPrime, generateLargePrime, generateTwoLargeDistinctPrimes, xgcd, 
 
 BigInt.prototype.toJSON = function () { return this.toString(); }
 
-const zkout = '../circuits/1024/out';
-
 const w = 121;
-const nBits = 1024;
-const bp = {
-    2: 782,
-    3: 661,
-};
 const M = 3;
+const ba = {
+    1024: 10,
+    2048: 18,
+    3072: 27,
+};
+const bp = {
+    1024: 7,
+    2048: 15,
+    3072: 24,
+};
+const bAcc = {
+    1024: 9,
+    2048: 17,
+    3072: 26,
+};
+const bpBits = {
+    1024: {
+        2: 782,
+        3: 661,
+    },
+    2048: {
+        2: 1806,
+        3: 1685,
+    },
+    3072: {
+        2: 2830,
+        3: 2709,
+    },
+};
 
-const mimc = mkMimc(`../circuits/out/MiMCHasher_js/MiMCHasher.wasm`, `../circuits/out/MiMCHasher_0001.zkey`);
+const mkCircuitPath = nBits => path.join(__dirname, `../circuits/${nBits}/out`);
+const mimcPath = path.join(__dirname, '../circuits/out');
+const outJsonPath = path.join(__dirname, '../json');
+
+const mimc = mkMimc(`${mimcPath}/MiMCHasher_js/MiMCHasher.wasm`, `${mimcPath}/MiMCHasher_0001.zkey`);
+
+function t() {
+    const now = new Date();
+    const ti = [now.getHours(), now.getMinutes(), now.getSeconds()]
+                    .map(x => x.toString())
+                    .map(x => x.padStart(2, '0'))
+                    .join('');
+    return ti;
+}
 
 async function generateNote(v) {
     const sn = await generateLargePrime(w);
@@ -33,7 +69,9 @@ async function generateNote(v) {
     }
 }
 
-async function testSend(ANote, availableNotes, vArr) {
+async function testSend(nBits, ANote, availableNotes, vArr) {
+    console.time('InitSend time');
+
     vArr = vArr.map(BigInt);
 
     const NOutNote = vArr.length;
@@ -46,7 +84,7 @@ async function testSend(ANote, availableNotes, vArr) {
         outNotes.push(n);
         availableNotes.push(n);
     }
-    const obfsPrime = await generateLargePrime(bp[NOutNote]);
+    const obfsPrime = await generateLargePrime(bpBits[nBits][NOutNote]);
 
     console.log('new notes: ', outNotes);
     console.log('obfsPrime: ', obfsPrime);
@@ -56,7 +94,7 @@ async function testSend(ANote, availableNotes, vArr) {
     console.log('aggNote: ', aggNote);
 
     const input = {
-        aggNote: numberToArray(aggNote, w, 10),
+        aggNote: numberToArray(aggNote, w, ba[nBits]),
         vIn,
         vOutNote,
         prover: 0, // omitted
@@ -64,18 +102,25 @@ async function testSend(ANote, availableNotes, vArr) {
         LOutNoteRd: arrayPad(outNotes.map(x => x.note.rd), M, 0n),
         LOutNoteSn: arrayPad(outNotes.map(x => x.note.sn), M, 0n),
         NOutNote,
-        obfsPrime: numberToArray(obfsPrime, w, 7),
+        obfsPrime: numberToArray(obfsPrime, w, bp[nBits]),
     };
 
-    fs.writeFileSync('../json/inputSend.json', JSON.stringify(input, null, 2), 'utf-8');
+    console.time('proof for Send time');
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+        input,
+        `${mkCircuitPath(nBits)}/Send_js/Send.wasm`,
+        `${mkCircuitPath(nBits)}/Send_0001.zkey`
+    );
+    console.timeEnd('proof for Send time');
 
-    console.time('proof for Send');
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, `${zkout}/Send_js/Send.wasm`, `${zkout}/Send_0001.zkey`);
-    console.timeEnd('proof for Send');
+    console.timeEnd('InitSend time');
 
-    console.log('proof for Send: ', proof, '\npublicSignals: ', publicSignals);
+    const ti = t();
+    fs.writeFileSync(`${outJsonPath}/inputSend_${nBits}_${ti}.json`, JSON.stringify(input, null, 2), 'utf-8');
+    fs.writeFileSync(`${outJsonPath}/proofSend_${nBits}_${ti}.json`, JSON.stringify(proof, null, 2), 'utf-8');
+    fs.writeFileSync(`${outJsonPath}/publicSend_${nBits}_${ti}.json`, JSON.stringify(publicSignals, null, 2), 'utf-8');
 
-    const res = await snarkjs.groth16.verify(JSON.parse(fs.readFileSync(`${zkout}/Send_vk.json`)), publicSignals, proof);
+    const res = await snarkjs.groth16.verify(JSON.parse(fs.readFileSync(`${mkCircuitPath(nBits)}/Send_vk.json`)), publicSignals, proof);
 
     if (res === true) {
         console.log("Verification OK");
@@ -86,7 +131,9 @@ async function testSend(ANote, availableNotes, vArr) {
     ANote.add(aggNote);
 }
 
-async function testReceive(ANote, ASn, availableNotes, inNotes, vArr, vOut) {
+async function testReceive(nBits, ANote, ASn, availableNotes, inNotes, vArr, vOut) {
+    console.time('InitReceive time');
+
     vArr = vArr.map(BigInt);
 
     const NInNote = inNotes.length;
@@ -104,8 +151,8 @@ async function testReceive(ANote, ASn, availableNotes, inNotes, vArr, vOut) {
     console.log('out notes: ', outNotes);
 
     const obfsPrimeArr = [
-        await generateLargePrime(bp[NInNote]), // for ASn
-        await generateLargePrime(bp[NOutNote]), // for ANote
+        await generateLargePrime(bpBits[nBits][NInNote]), // for ASn
+        await generateLargePrime(bpBits[nBits][NOutNote]), // for ANote
     ];
 
     console.log('obfsPrime: ', obfsPrimeArr);
@@ -123,14 +170,14 @@ async function testReceive(ANote, ASn, availableNotes, inNotes, vArr, vOut) {
     console.log('non-membership proof of ASn: ', [PSn_a, PSn_B]);
 
     const input = {
-        modulusANote: numberToArray(ANote.n, w, 9),
-        ANote: numberToArray(ANote.A, w, 9),
-        aggNote: numberToArray(aggNote, w, 10),
+        modulusANote: numberToArray(ANote.n, w, bAcc[nBits]),
+        ANote: numberToArray(ANote.A, w, bAcc[nBits]),
+        aggNote: numberToArray(aggNote, w, ba[nBits]),
 
-        gASn: numberToArray(ASn.g, w, 9),
-        modulusASn: numberToArray(ASn.n, w, 9),
-        ASn: numberToArray(ASn.A, w, 9),
-        aggSn: numberToArray(aggSn, w, 10),
+        gASn: numberToArray(ASn.g, w, bAcc[nBits]),
+        modulusASn: numberToArray(ASn.n, w, bAcc[nBits]),
+        ASn: numberToArray(ASn.A, w, bAcc[nBits]),
+        aggSn: numberToArray(aggSn, w, ba[nBits]),
 
         vOut,
         prover: 0, // omitted
@@ -145,22 +192,29 @@ async function testReceive(ANote, ASn, availableNotes, inNotes, vArr, vOut) {
         LOutNoteSn: arrayPad(outNotes.map(x => x.note.sn), M, 0n),
         NOutNote,
 
-        PNote: numberToArray(PNote, w, 9),
+        PNote: numberToArray(PNote, w, bAcc[nBits]),
         PSn_a: numberToArray(PSn_a, w, M),
-        PSn_B: numberToArray(PSn_B, w, 9),
+        PSn_B: numberToArray(PSn_B, w, bAcc[nBits]),
 
-        obfsPrime: obfsPrimeArr.map(x => numberToArray(x, w, 7)),
+        obfsPrime: obfsPrimeArr.map(x => numberToArray(x, w, bp[nBits])),
     }
 
-    fs.writeFileSync('../json/inputReceive.json', JSON.stringify(input, null, 2), 'utf-8');
+    console.time('proof for Receive time');
+    const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+        input,
+        `${mkCircuitPath(nBits)}/Receive_js/Receive.wasm`,
+        `${mkCircuitPath(nBits)}/Receive_0001.zkey`
+    );
+    console.timeEnd('proof for Receive time');
 
-    console.time('proof for Receive');
-    const { proof, publicSignals } = await snarkjs.groth16.fullProve(input, `${zkout}/Receive_js/Receive.wasm`, `${zkout}/Receive_0001.zkey`);
-    console.timeEnd('proof for Receive');
+    console.timeEnd('InitReceive time');
 
-    console.log('proof for Receive: ', proof, '\npublicSignals: ', publicSignals);
+    const ti = t();
+    fs.writeFileSync(`${outJsonPath}/inputReceive_${nBits}_${ti}.json`, JSON.stringify(input, null, 2), 'utf-8');
+    fs.writeFileSync(`${outJsonPath}/proofReceive_${nBits}_${ti}.json`, JSON.stringify(proof, null, 2), 'utf-8');
+    fs.writeFileSync(`${outJsonPath}/publicReceive_${nBits}_${ti}.json`, JSON.stringify(publicSignals, null, 2), 'utf-8');
 
-    const res = await snarkjs.groth16.verify(JSON.parse(fs.readFileSync(`${zkout}/Receive_vk.json`)), publicSignals, proof);
+    const res = await snarkjs.groth16.verify(JSON.parse(fs.readFileSync(`${mkCircuitPath(nBits)}/Receive_vk.json`)), publicSignals, proof);
 
     if (res === true) {
         console.log("Verification OK");
@@ -173,20 +227,29 @@ async function testReceive(ANote, ASn, availableNotes, inNotes, vArr, vOut) {
 }
 
 async function main() {
-    const ANote = await RsaAcc.setup(nBits);
-    const ASn = await RsaAcc.setup(nBits);
-    const availableNotes = [];
+    for (const nBits of [1024, 2048, 3072]) {
+        console.log(`!!! nBits: ${nBits} !!!`);
 
-    console.log('=== Test: Send, in 30 coins, out 3 notes (10, 10, 10) ===');
-    await testSend(ANote, availableNotes, [10n, 10n, 10n]);
+        console.time('accumulator setup time');
+        const ANote = await RsaAcc.setup(nBits);
+        const ASn = await RsaAcc.setup(nBits);
+        console.timeEnd('accumulator setup time');
 
-    console.log('\n=== Test: Receive, in 2 notes (10, 10), out 2 notes (7, 8) + 5 coins ===');
-    let inNotes = [availableNotes.pop(), availableNotes.pop()];
-    await testReceive(ANote, ASn, availableNotes, inNotes, [7n, 8n], 5n);
+        const availableNotes = [];
 
-    console.log('\n=== Test: Receive, in 3 notes (8, 7, 10), out 3 notes (0, 0, 1) + 24 coins ===');
-    inNotes = [availableNotes.pop(), availableNotes.pop(), availableNotes.pop()];
-    await testReceive(ANote, ASn, availableNotes, inNotes, [0n, 0n, 1n], 24n);
+        console.log('=== Test: Send, in 30 coins, out 3 notes (10, 10, 10) ===');
+        await testSend(nBits, ANote, availableNotes, [10n, 10n, 10n]);
+
+        console.log('\n=== Test: Receive, in 2 notes (10, 10), out 2 notes (7, 8) + 5 coins ===');
+        let inNotes = [availableNotes.pop(), availableNotes.pop()];
+        await testReceive(nBits, ANote, ASn, availableNotes, inNotes, [7n, 8n], 5n);
+
+        console.log('\n=== Test: Receive, in 3 notes (8, 7, 10), out 3 notes (0, 0, 1) + 24 coins ===');
+        inNotes = [availableNotes.pop(), availableNotes.pop(), availableNotes.pop()];
+        await testReceive(nBits, ANote, ASn, availableNotes, inNotes, [0n, 0n, 1n], 24n);
+
+        console.log('\n');
+    }
 }
 
 main().then(() => {
